1b5798d997770968ea20c67d97f2f6b162dcfad1
Bertrand <bertrand.guiral@gmail.com>
2016-09-03 08:20:57 +0200

    bg

--- test/bg/client/test/LexiqueGwtTest.java
+++ test/bg/client/test/LexiqueGwtTest.java
@@ -1,40 +1,84 @@
 package bg.client.test;
 
+
 import org.junit.Test;
 
+import bg.client.inter.sigale.model.ISigalePropertes;
 import bg.client.inter.sigale.model.Lexique;
 import bg.client.inter.sigale.model.LexiqueFactory;
-import bg.client.inter.sigale.model.statistic.StatistiquesLexique;
-import bg.client.inter.sigale.model.statistic.StatistiquesLexiqueFactory;
+import bg.client.inter.sigale.util.ILogListener;
 
+import com.google.gwt.core.shared.GWT;
 import com.google.gwt.junit.client.GWTTestCase;
 
+
+
+/**
+ * Pour lancer les tests JUnit en "Production Mode"
+ * Essayé en simple java, ne marche pas: Trop de dépendances gwt (outre GWTTestCase ).
+ * 
+ * @author utilisateur
+ *
+ */
 public class LexiqueGwtTest extends GWTTestCase {
-
-	@Test
-	public void test_0_LexiqueFactory() {
+	ISigalePropertes sigaleProperties = new ISigalePropertes() {
 		
-		Lexique lexique = LexiqueFactory.getInstance().getLexique();
-		String xml = LexiqueFactory.getInstance().toXml(lexique);
-		System.out.println("lexique xml : "+xml);
-		Lexique lexique2 = LexiqueFactory.getInstance().parse(xml);
-		GWTTestCase.assertEquals(lexique.getName(), lexique2.getName());
-		GWTTestCase.assertEquals(lexique.getListUniteLexicale().size(), lexique2.getListUniteLexicale().size());
-		GWTTestCase.assertEquals(lexique, lexique2);
+		@Override
+		public void setNameLexique(String nameLexique) {
+			
+		}
 		
-	}
+		@Override
+		public String getNameLexique() {
+			
+			return null;
+		}
+	};
+	ILogListener logListener = new ILogListener() {
+		
+		@Override
+		public void logTitle(String s) {
+			System.out.println(s);
+		}
+		
+		@Override
+		public void logText(String message) {
+			System.out.println(message);
+			
+		}
+		
+		@Override
+		public void log(String message, Throwable t) {
+			System.out.println(message);
+			t.printStackTrace();
+		}
+		
+		@Override
+		public void log(String message) {
+			System.out.println(message);
+		}
+	};
 	
 	
 	@Test
-	public void test_1_StatistiqueFactory() {
+	public void test0() {
 		
-		StatistiquesLexique stat = StatistiquesLexiqueFactory.getInstance().getStatistiquesLexique();
-		GWTTestCase.assertNotNull(stat);
-		String xml = StatistiquesLexiqueFactory.getInstance().toXML(stat);
-		System.out.println("stat.xml : "+xml);
-		StatistiquesLexique stat2 = StatistiquesLexiqueFactory.getInstance().parse(xml);
+		GWT.log("TEst de Junit");
+		GWTTestCase.assertTrue(true);
 	}
-
+		
+	@Test
+   public void test() {
+		
+		LexiqueFactory lexiqueFactory = new LexiqueFactory(sigaleProperties,logListener);
+		Lexique lexique_1 = lexiqueFactory.getLexique();
+		String xml_1 = lexiqueFactory.toXml(lexique_1);
+		GWT.log("xml "+xml_1);
+		Lexique lexique_2 = lexiqueFactory.parse(xml_1);
+		GWTTestCase.assertTrue(lexique_1.equals(lexique_2));
+		
+	}
+	
 
 	@Override
 	public String getModuleName() {
